439343a37bf5aebbe8522545af33346a83053620
J0ax771zz <moscardicommerce@gmail.com>
2026-01-20 18:59:55 -0300

    Importing and using CSS Modules on React

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import Heading from './components/Heading';
+
 import './styles/theme.css'
 import './styles/global.css'
+
 
 function App(){
     return(
         <>
-        <h1>Hello World</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis ipsa quae, repudiandae eaque incidunt exercitationem quibusdam veniam nostrum a magnam, officia voluptas? Incidunt sapiente aliquid harum ea consectetur. Voluptas.</p>
+        <Heading />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis ipsa quae, 
+            repudiandae eaque incidunt exercitationem quibusdam veniam nostrum a magnam, officia 
+            voluptas? Incidunt sapiente aliquid harum ea consectetur. Voluptas.
+        </p>
         </>
     );
 }
