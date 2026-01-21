7cf3672b0cc89038ad836ffe8a266f2aa717ab89
J0ax771zz <moscardicommerce@gmail.com>
2026-01-21 19:41:13 -0300

    Creating our HTML and CSS structure for each section of our app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import Heading from './components/Heading';
+import "./styles/theme.css";
+import "./styles/global.css";
 
-import './styles/theme.css'
-import './styles/global.css'
-import { Timer, TimerIcon } from 'lucide-react';
-
-
-
-function App(){
-    return(
-        <>
-        <Heading>
-            Hello, World
-            <button>
-                <TimerIcon />
-            </button>
-        </Heading>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis ipsa quae, 
-            repudiandae eaque incidunt exercitationem quibusdam veniam nostrum a magnam, officia 
-            voluptas? Incidunt sapiente aliquid harum ea consectetur. Voluptas.
-        </p>
-        </>
-    );
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            MENU
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            FORM
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            FOOTER
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
