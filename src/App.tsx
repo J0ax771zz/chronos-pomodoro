19e775215f05960e212ba89c092b18502a2dd2ed
J0ax771zz <moscardicommerce@gmail.com>
2026-01-25 16:19:24 -0300

    Creating the menu component with links and icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./styles/global.css";
 import Heading from "./components/Heading";
 import Container from "./components/Container";
 import Logo from "./components/Logo";
+import Menu from "./components/Menu";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
