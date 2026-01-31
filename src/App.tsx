8524a4efdb569a7df1586bc70617de7b4f9f78bf
J0ax771zz <moscardicommerce@gmail.com>
2026-01-31 13:43:11 -0300

    Creating the form of the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-import Heading from "./components/Heading";
 import Container from "./components/Container";
 import Logo from "./components/Logo";
 import Menu from "./components/Menu";
@@ -20,6 +19,31 @@ function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form action="" className="form">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input id="input" type="text" />
+          </div>
+          <div className="formRow">
+            <p>
+              Lorem ipsum dolor sit amet.
+            </p>
+          </div>
+          <div className="formRow">
+            <p>
+              Ciclos
+            </p>
+            <p>
+              0 0 0 0 0 0
+            </p>
+          </div>
+          <div className="formRow">
+           <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
