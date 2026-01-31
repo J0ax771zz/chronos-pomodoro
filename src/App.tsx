b1ea020b9d0e59a24514078abbda56fe01999e02
J0ax771zz <moscardicommerce@gmail.com>
2026-01-31 14:38:22 -0300

    adding the style for the input component and the states of the component like: active, focus and disabled

--- src/App.tsx
+++ src/App.tsx
@@ -24,24 +24,26 @@ function App() {
 
       <Container>
         <form action="" className="form">
+          {/* Div de input da tarefa */}
           <div className="formRow">
-            <DefaultInput id="meuInput" labelText="task" type='text'/>
+            <DefaultInput
+              id="meuInput"
+              labelText="task"
+              type="text"
+              placeholder="Digite algo"
+            />
+          {/* Div de  da tarefa */}
           </div>
           <div className="formRow">
-            <p>
-              Lorem ipsum dolor sit amet.
-            </p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          {/* Div de ciclos da tarefa */}
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
           </div>
           <div className="formRow">
-            <p>
-              Ciclos
-            </p>
-            <p>
-              0 0 0 0 0 0
-            </p>
-          </div>
-          <div className="formRow">
-           <button>Enviar</button>
+            <button>Enviar</button>
           </div>
         </form>
       </Container>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css'
+
 type DefaultInputProps = {
   id: string;
   labelText: string;
@@ -14,7 +16,7 @@ function DefaultInput({
       {/* Adding a conditonal option for the text/tittle of the input */}
       {/* {labelText && <label htmlFor={id}>{labelText}</label>} */}
       <label htmlFor={id}>{labelText}</label>
-      <input id={id} type={type} {...rest} />
+      <input className={styles.input} id={id} type={type} {...rest} />
     </>
   );
 }
