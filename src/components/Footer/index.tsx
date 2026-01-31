00975bcf32fe02bdff2e491f589334e87bba2ae8
J0ax771zz <moscardicommerce@gmail.com>
2026-01-31 18:20:03 -0300

    Creating the Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css'
+
+
+
+function Footer() {
+    return <footer className={styles.footer}>
+        <a href="">
+            Entenda como funciona a técnica pomodoro
+        </a>
+        <a href="">
+            Chronos Pomodoro &copy; {new Date().getFullYear()} - Feito com 💚
+        </a>
+    </footer>;
+}
+
+export default Footer;
