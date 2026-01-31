3d2bc70db9a99e0d7b15ddfe60ee9c95ed8cfe55
J0ax771zz <moscardicommerce@gmail.com>
2026-01-31 14:01:22 -0300

    Starting the Input Component and addinng the starting configs

---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,16 @@
+import type React from "react";
+
+type DefaultInputProps = {
+    id: string;
+} & React.ComponentProps<'input'>
+
+function DefaultInput({id, type } : DefaultInputProps) {
+    return(
+        <>
+        <label htmlFor={id}>task</label>
+        <input id={id} type={type} />
+        </>
+    );
+}
+
+export default DefaultInput;
